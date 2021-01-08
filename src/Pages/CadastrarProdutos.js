742faf5ca6b10034765d3a5d7236c61b9536b411
Tarcisio Coelho <taarcisioaraujo@gmail.com>
2021-01-08 16:26:42 -0300

    atualizado puxando os dados do banco

--- src/Pages/CadastrarProdutos.js
+++ src/Pages/CadastrarProdutos.js
@@ -2,13 +2,27 @@ import Menu from "../Componentes/Menu/Menu"
 import './Css/CadastrarProdutos.css';
 
 export default function CadastrarProdutos() {
+    const cadastrarProdutos = async(evento) => {
+        
+        const url = "http://localhost/API/action/cadastrarproduto.php";
+        const dados = new FormData(evento.target)
+        const cabecalho = {
+            method: 'POST',
+            body: dados
+        }
+
+        const resposta = await fetch(url, cabecalho);
+        const retorno = await resposta.json();
+        console.log(retorno);
+    }
+
     return (
         <>
             <Menu/>
             <div className="container">
                 <div className="mt-5 mb-5 p-3 border box-cadastro">
                         <h1 className="text-center text-white">Cadastrar Produto</h1><hr></hr>
-                    <form className="form-group">
+                    <form action="/produtos" className="form-group" onSubmit={cadastrarProdutos}>
                         <div className="mb-3">
                             <label for="nome" className="form-label text-white">PRODUTO</label>
                             <input type="text" className="form-control" id="produto" name="produto" />
@@ -24,15 +38,30 @@ export default function CadastrarProdutos() {
                         </div>
                         
                         <div className="mb-3">
-                            <label for="formFileSm" className="form-label text-white">IMAGEM</label>
-                            <input className="form-control form-control-sm" id="formFileSm" type="file" />
+                            <label className="form-label text-white">IMAGEM</label>
+                            <input type="file" name="imagem" />
                         </div>
+
+                        <div className="mb-3"> 
+                            <label className="form-label text-white">VALOR</label>
+                            <div className="input-group mb-3">
+                                <input type="text" name="valor" className="form-control" placeholder="PREÇO DO PRODUTO" aria-label="Username" aria-describedby="basic-addon1" />
+                                <span className="input-group-text" id="basic-addon1">$</span>
+                            </div>
+                        </div>
+                        
+                        <div className="mb-3">
+                            <label className="form-label text-white">DESCRIÇÃO</label>
+                            <div className="form-floating">
+                                <textarea className="form-control textarea" name="descricao" id="descricao"></textarea>
+                            </div>
+                        </div>
+
                         <div className="text-center">
-                            <button className="btn btn-primary" type="submit">Enviar</button>
+                            <button className="btn btn-primary" name="enviar" type="submit">Enviar</button>
                         </div>
                     </form>
                 </div>
-
             </div>
             
         </>
--- src/Pages/Produtos.js
+++ src/Pages/Produtos.js
@@ -1,67 +1,36 @@
 import Menu from "../Componentes/Menu/Menu"
+import IndexProduto from "../Componentes/Produtos/IndexProdutos";
 import './Css/Produtos.css';
+import React from 'react';
 
 const Produtos = () => {
+    const [action, setAction] = React.useState('');
+    const [user, setUser] = React.useState('');
+    
+    async function pegarProdutos(){
+        const url = await fetch('http://localhost/API/api/produtos.php');
+        const urlUser = await fetch('http://localhost/API/api/user.php');
+        
+        const urlResponse = await url.json();
+        const userResponse = await urlUser.json();
+
+
+        setAction(urlResponse);
+        console.log(action);
+
+        setUser(userResponse);
+        console.log(user);
+    }
+
     return (
         <>
             <Menu/>
             <div className="container-fluid">
                 <h3 className="text-center mt-3">Selecionar categoria aqui</h3>
                 <div className="d-flex box">
-                    <div className="card mt-5 m-3 border border-dark">
-                        <img src={require('../Imagens/default.jpg').default} className="card-img-top" alt="..." />
-                        <div className="card-body">
-                            <h5 className="card-title">Produtos de Limpeza</h5>
-                            <p className="card-text">Anunciado por: </p>
-                            <p className="card-text">Descrição do produto </p>
-                        </div>
-                    </div>
-
-                    <div className="card mt-5 m-3 border border-dark">
-                        <img src={require('../Imagens/default.jpg').default} className="card-img-top" alt="..." />
-                        <div className="card-body">
-                            <h5 className="card-title">Produtos de Limpeza</h5>
-                            <p className="card-text">Anunciado por: </p>
-                            <p className="card-text">Descrição do produto </p>
-                        </div>
-                    </div>
-
-                    <div className="card mt-5 m-3 border border-dark">
-                        <img src={require('../Imagens/default.jpg').default} className="card-img-top" alt="..." />
-                        <div className="card-body">
-                            <h5 className="card-title">Produtos de Limpeza</h5>
-                            <p className="card-text">Anunciado por: </p>
-                            <p className="card-text">Descrição do produto </p>
-                        </div>
-                    </div>
-
-                    <div className="card mt-5 m-3 border border-dark">
-                        <img src={require('../Imagens/default.jpg').default} className="card-img-top" alt="..." />
-                        <div className="card-body">
-                            <h5 className="card-title">Produtos de Limpeza</h5>
-                            <p className="card-text">Anunciado por: </p>
-                            <p className="card-text">Descrição do produto </p>
-                        </div>
-                    </div>
-
-                    <div className="card mt-5 m-3 border border-dark">
-                        <img src={require('../Imagens/default.jpg').default} className="card-img-top" alt="..." />
-                        <div className="card-body">
-                            <h5 className="card-title">Produtos de Limpeza</h5>
-                            <p className="card-text">Anunciado por: </p>
-                            <p className="card-text">Descrição do produto </p>
-                        </div>
-                    </div>
-
-                    <div className="card mt-5 m-3 border border-dark">
-                        <img src={require('../Imagens/default.jpg').default} className="card-img-top" alt="..." />
-                        <div className="card-body">
-                            <h5 className="card-title">Produtos de Limpeza</h5>
-                            <p className="card-text">Anunciado por: </p>
-                            <p className="card-text">Descrição do produto </p>
-                        </div>
-                    </div>
-                </div>
+                    {action && action.map(mapear => <IndexProduto categoria={mapear.categoria} produto={mapear.produto} valor={mapear.valor} descricao={mapear.descricao} anunciante={mapear.nome} />)}    
+                </div>        
+                <button onClick={pegarProdutos}>Ver</button>
             </div>
         </>
     )
